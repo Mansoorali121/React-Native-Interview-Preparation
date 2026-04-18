b2c3a6d946ddc50d16857f85cf8a804761cfea82
Mansoor Ali <mansoorsahito601@gmail.com>
2026-04-18 09:18:31 +0500

    Email Validation App

--- App.tsx
+++ App.tsx
@@ -73,6 +73,77 @@
 //   },
 // });
 
+// import {
+//   StyleSheet,
+//   Text,
+//   TextInput,
+//   TouchableOpacity,
+//   View,
+// } from 'react-native';
+// import React, { useState } from 'react';
+
+// const App = () => {
+//   const [age, setAge] = useState('');
+//   const [message, setMessage] = useState('');
+
+//   const checkAge = () => {
+//     if (age.trim() === '') {
+//       setMessage('Please enter age ❌');
+//       return;
+//     }
+
+//     const numAge = Number(age);
+
+//     if (isNaN(numAge)) {
+//       setMessage('Invalid age ❌');
+//     } else if (numAge >= 18) {
+//       setMessage('You can vote ✅');
+//     } else {
+//       setMessage('You are underage ❌');
+//     }
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>Voting Eligibility App</Text>
+
+//       <TextInput
+//         placeholder="Enter your age"
+//         style={styles.input}
+//         keyboardType="numeric"
+//         value={age}
+//         onChangeText={setAge}
+//       />
+
+//       <TouchableOpacity style={styles.button} onPress={checkAge}>
+//         <Text style={styles.btnText}>Check Age</Text>
+//       </TouchableOpacity>
+
+//       <Text style={styles.message}>{message}</Text>
+//     </View>
+//   );
+// };
+
+// export default App;
+
+// const styles = StyleSheet.create({
+//   container: { flex: 1, justifyContent: 'center', padding: 20 },
+//   title: { fontSize: 20, fontWeight: 'bold', marginBottom: 20 },
+//   input: {
+//     borderWidth: 1,
+//     padding: 10,
+//     marginBottom: 15,
+//     borderRadius: 5,
+//   },
+//   button: {
+//     backgroundColor: 'green',
+//     padding: 10,
+//     alignItems: 'center',
+//     borderRadius: 10,
+//   },
+//   btnText: { color: '#fff', fontWeight: 'bold' },
+//   message: { marginTop: 20, fontSize: 18 },
+// });
 
 import {
   StyleSheet,
@@ -84,43 +155,36 @@ import {
 import React, { useState } from 'react';
 
 const App = () => {
-  const [age, setAge] = useState('');
+  const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  // Email Validation Function
 
-  const checkAge = () => {
-    if (age.trim() === '') {
-      setMessage('Please enter age ❌');
-      return;
-    }
+  const validateEmail = () => {
+    const trimedEmail = email.trim();
 
-    const numAge = Number(age);
-
-    if (isNaN(numAge)) {
-      setMessage('Invalid age ❌');
-    } else if (numAge >= 18) {
-      setMessage('You can vote ✅');
+    if (trimedEmail === '') {
+      setMessage('Please enter email');
+    } else if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(trimedEmail)) {
+      setMessage('Valid Email');
     } else {
-      setMessage('You are underage ❌');
+      setMessage('Invalid Email');
     }
   };
-
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Voting Eligibility App</Text>
-
-      <TextInput
-        placeholder="Enter your age"
-        style={styles.input}
-        keyboardType="numeric"
-        value={age}
-        onChangeText={setAge}
-      />
-
-      <TouchableOpacity style={styles.button} onPress={checkAge}>
-        <Text style={styles.btnText}>Check Age</Text>
-      </TouchableOpacity>
-
-      <Text style={styles.message}>{message}</Text>
+      <Text style={styles.title}>Email Validation App </Text>
+      <View style={styles.formcontainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+        />
+        <TouchableOpacity style={styles.button} onPress={validateEmail}>
+          <Text style={styles.btntext}>Check Email </Text>
+        </TouchableOpacity>
+        <Text style={styles.message}>{message}</Text>
+      </View>
     </View>
   );
 };
@@ -128,20 +192,16 @@ const App = () => {
 export default App;
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', padding: 20 },
+  container: { flex: 1, alignItems: 'center', marginTop: 20 },
   title: { fontSize: 20, fontWeight: 'bold', marginBottom: 20 },
-  input: {
-    borderWidth: 1,
-    padding: 10,
-    marginBottom: 15,
-    borderRadius: 5,
-  },
+  formcontainer: { marginTop: 20, gap: 20 },
+  input: { borderWidth: 1, padding: 10, width: 230, borderRadius: 5 },
   button: {
     backgroundColor: 'green',
-    padding: 10,
+    padding: 15,
     alignItems: 'center',
     borderRadius: 10,
   },
-  btnText: { color: '#fff', fontWeight: 'bold' },
-  message: { marginTop: 20, fontSize: 18 },
+  btntext: { color: '#fff', fontWeight: 'bold' },
+  message:{fontSize:20,color:"red"},
 });
