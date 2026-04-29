dd65ed9add7dfd579af451884caf99151c570cba
Mansoor Ali <mansoorsahito601@gmail.com>
2026-04-29 11:36:57 +0500

    APIS

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 const App = () => {
   return (
-    <View>
-      <Text>React Native Local Storage </Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Third party APIS  </Text>
     </View>
-  )
-}
+  );
+};
 
 export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: 'center', marginTop: 30 },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'red',
+    fontFamily: 'Arial',
+  },
+});
