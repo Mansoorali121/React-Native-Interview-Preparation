3a8505982ec31720163ba314cf74298131ad1b5e
Mansoor Ali <mansoorsahito601@gmail.com>
2026-05-02 09:25:44 +0500

    book component

---
+++ src/components/Book.js
@@ -0,0 +1,36 @@
+import { Image, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const Book = ({ author, nameOfBook, price, coverURL, categoryColor }) => {
+  return (
+    <View style={styles.container}>
+      <View style={[styles.coloredSquare, { backgroundColor: 'red' }]}>
+        <Image source={{ uri: coberURL }} style={styles.image} />
+      </View>
+      <Text>{author}</Text>
+      <Text>{nameeOfBook}</Text>
+      <Text>{price}$</Text>
+    </View>
+  );
+};
+
+export default Book;
+
+const styles = StyleSheet.create({
+  container: { alignItems: 'center', marginTop: 50 },
+  coloredSquare: {
+    borderRadius: 8,
+    height: 130,
+    width: 130,
+    alignItems: 'center',
+    marginBottom: 5,
+  },
+  image: {
+    height: 130,
+    width: 80,
+    resizeMode: 'stretch',
+    backgroundColor: 'blue',
+    marginTop: -20,
+    borderRadius: 8,
+  },
+});
