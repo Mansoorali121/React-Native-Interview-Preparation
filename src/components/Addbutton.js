2923e4dd0cf4126d0f54dd04c4c52f30ec324f31
Mansoor Ali <mansoorsahito601@gmail.com>
2026-05-04 10:57:34 +0500

    add button UI design

--- src/components/Addbutton.js
+++ src/components/Addbutton.js
@@ -1,14 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import { colors } from '../style/colors';
 
 const Addbutton = () => {
   return (
-    <View>
-      <Text>Addbutton</Text>
-    </View>
-  )
-}
+    <TouchableOpacity style={styles.container}>
+      <Image source={require('../assets/add.png')} style={styles.image} />
+    </TouchableOpacity>
+  );
+};
 
 export default Addbutton;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  image: { height: 20, width: 20 },
+  container: {
+    position: 'absolute',
+    bottom: 50,
+    alignSelf: 'center',
+    backgroundColor: colors.mainCOlor,
+    zIndex: 1,
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
---
+++ src/style/colors.ts
@@ -0,0 +1,7 @@
+export enum colors  {
+    lightGray = "#F1F1F1",
+    grayText = "#A2A2A2",
+    white = "#FFFFFF",
+    blackText = "#121212",
+    mainCOlor = "#0277FA",
+}
