2d284693901c8130505e71160dc44a1c0a6dc7bb
Mansoor Ali <mansoorsahito601@gmail.com>
2026-05-04 11:20:13 +0500

    coustom input

---
+++ src/components/Maininput.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Maininput = () => {
+  return (
+    <View>
+      <Text>Maininput</Text>
+    </View>
+  )
+}
+
+export default Maininput;
+
+const styles = StyleSheet.create({})
