3569d7206104cf2d927ea3f7c084ca2c7128e514
Mansoor Ali <mansoorsahito601@gmail.com>
2026-05-07 09:59:10 +0500

    Advance UI Design

--- App.js
+++ App.js
@@ -1,31 +1,14 @@
-import { Alert, Button, FlatList, StyleSheet, Text, View } from 'react-native';
-import React, { memo, useEffect, useState } from 'react';
-import axios from 'axios';
-import Home from './src/screens/Home';
-import Maininput from "./src/components/Maininput";
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
 const App = () => {
-  const [value,setValue] = useState("");
-
-
-
   return (
-    <View style={styles.container}>
-      <Home />
-      {/* <Maininput options={{value:value, onChangeText:(t)=> setValue(t)}} />
-        <Text>{value}</Text> */}
+    <View>
+      <Text>App</Text>
     </View>
-  );
-};
+  )
+}
 
 export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    marginTop: 30,
-    marginBottom: 30,
-    paddingHorizontal: 30,
-  },
-});
+const styles = StyleSheet.create({})
