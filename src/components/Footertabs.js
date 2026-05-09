e4777b8177184d866c8a1dbbc8c343a14382a647
Mansoor Ali <mansoorsahito601@gmail.com>
2026-05-09 12:34:56 +0500

    tabs array added

--- src/components/Footertabs.js
+++ src/components/Footertabs.js
@@ -1,6 +1,6 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
-import { s } from 'react-native-size-matters';
+import { s, vs } from 'react-native-size-matters';
 
 // Tabs Array
 const tabsArr = ["All","In Progress","Completed","Overdue"];
@@ -11,7 +11,14 @@ const Active_Text = "#FFFFF";
 const Footertabs = () => {
   return (
     <View style={styles.container}>
-      <Text>Footertabs</Text>
+      {
+        tabsArr.map((tabName)=>{
+          return <TouchableOpacity style={styles.tabbutton}>
+            <Text>{tabName}</Text>
+
+          </TouchableOpacity>
+        })
+      }
     </View>
   )
 }
@@ -21,6 +28,17 @@ export default Footertabs;
 const styles = StyleSheet.create({
     container:{backgroundColor:"#F5F5F5",
         borderRadius:s(12),
+        height:vs(48),
+        flexDirection:"row",
+        marginTop:s(10),
+        
+
+    },
+    tabbutton:{height:vs(32),
+      flex:1,
+      borderRadius:s(8),
+      justifyContent:"center",
+      alignItems:"center",
 
     },
 })
