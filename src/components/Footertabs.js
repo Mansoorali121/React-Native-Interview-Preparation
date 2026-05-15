2973426ec3c3545b6c8b9c967367a28a905cb477
Mansoor Ali <mansoorsahito601@gmail.com>
2026-05-15 10:15:45 +0500

    tab bg

--- src/components/Footertabs.js
+++ src/components/Footertabs.js
@@ -1,47 +1,55 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, { useState } from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
 import { s, vs } from 'react-native-size-matters';
 
 // Tabs Array
-const tabsArr = ["All","In Progress","Completed","Overdue"];
+const tabsArr = ['All', 'In Progress', 'Completed', 'Overdue'];
 
-const Active_Bg="#75563B";
-const Active_Text = "red";
+const Active_Bg = '#6089bb';
+const Active_Text = 'red';
 
 const Footertabs = () => {
-  const [activetab,setActivetab] = useState("Completed");
+  const [activetab, setActivetab] = useState('Completed');
   return (
     <View style={styles.container}>
-      {
-        tabsArr.map((tabName)=>{
-          return <TouchableOpacity style={[styles.tabbutton,{}]}>
-            <Text>{tabName}</Text>
-
+      {tabsArr.map(tabName => {
+        return (
+          <TouchableOpacity onPress={}
+            style={[
+              styles.tabbutton,
+              activetab === tabName && { backgroundColor: Active_Bg },
+            ]}
+          >
+            <Text
+              style={[styles.text, activetab === tabName && { color: '#fff' }]}
+            >
+              {tabName}
+            </Text>
           </TouchableOpacity>
-        })
-      }
+        );
+      })}
     </View>
-  )
-}
+  );
+};
 
 export default Footertabs;
 
 const styles = StyleSheet.create({
-    container:{backgroundColor:"#F5F5F5",
-        borderRadius:s(12),
-        height:vs(48),
-        flexDirection:"row",
-        marginTop:s(10),
-        alignItems:"center",
-        padding:s(4)
-
-
-    },
-    tabbutton:{height:vs(32),
-      flex:1,
-      borderRadius:s(8),
-      justifyContent:"center",
-      alignItems:"center",
-
-    },
-})
+  container: {
+    backgroundColor: '#F5F5F5',
+    borderRadius: s(12),
+    height: vs(48),
+    flexDirection: 'row',
+    marginTop: s(10),
+    alignItems: 'center',
+    padding: s(4),
+  },
+  tabbutton: {
+    height: vs(32),
+    flex: 1,
+    borderRadius: s(8),
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: { color: 'black' },
+});
