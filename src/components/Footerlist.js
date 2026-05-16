cf1750a8b15938361cb4adc0c06218b89a4abbb6
Mansoor Ali <mansoorsahito601@gmail.com>
2026-05-16 12:36:05 +0500

    data with array

--- src/components/Footerlist.js
+++ src/components/Footerlist.js
@@ -1,33 +1,75 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { s, vs } from 'react-native-size-matters';
 
 const Footerlist = () => {
+  const data = [
+    {
+      id: 1,
+      title: 'Design New Home Screen',
+      description: 'Create UI for the new dashboard',
+      status: 'In Process',
+      backgroundColor: '',
+    },
+    {
+      id: 2,
+      title: 'Fix bug in authentication',
+      description: 'Resolve issues in login flow',
+      status: 'Completed',
+    },
+  ];
   return (
-    <View style={styles.cont}>
-      <View style={{ flexDirection: 'row' }}>
-        <View style={styles.circle}></View>
-        <View>
-          <Text style={styles.newtext}>Design New Home Screen </Text>
-          <Text style={styles.uitext}>Create UI for the new dashboard</Text>
-          <Text style={styles.protext}>In Progress</Text>
-        </View>
-        <View style={styles.date}>
-          <Image
-            style={styles.image}
-            source={require('../assets/calendar.png')}
-          />
-          <Text style={styles.datetext}>20 May </Text>
-        </View>
-      </View>
-    </View>
+    <FlatList
+      data={data}
+      keyExtractor={item => item.id}
+      renderItem={({ item }) => {
+        return (
+          <View style={styles.cont}>
+            <View style={{ flexDirection: 'row' }}>
+              <View
+                style={[
+                  styles.circle,
+                  item.id == 2 && { borderColor: 'orange' },
+                ]}
+              ></View>
+              <View>
+                <Text style={styles.newtext}>{item.title}</Text>
+                <Text style={styles.uitext}>{item.description}</Text>
+                <Text
+                  style={[
+                    styles.protext,
+                    item.id == 2 && { backgroundColor: 'orange' },
+                  ]}
+                >
+                  {item.status}
+                </Text>
+              </View>
+              <View style={styles.date}>
+                <Image
+                  style={styles.image}
+                  source={require('../assets/calendar.png')}
+                />
+                <Text style={styles.datetext}>20 May </Text>
+              </View>
+            </View>
+          </View>
+        );
+      }}
+    />
   );
 };
 
 export default Footerlist;
 
 const styles = StyleSheet.create({
-  cont: { height: vs(90), width: '100%', top: vs(10) , backgroundColor:"#e4e7ea", borderRadius:s(10),marginBottom:s(5)},
+  cont: {
+    height: vs(90),
+    width: '100%',
+    top: vs(10),
+    backgroundColor: '#e4e7ea',
+    borderRadius: s(10),
+    marginBottom: s(5),
+  },
   circle: {
     borderWidth: 2,
     borderColor: 'blue',
