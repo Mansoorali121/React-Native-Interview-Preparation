595c617bd0bebba4b320bc9334d2d00608deda0f
Mansoor Ali <mansoorsahito601@gmail.com>
2026-05-24 09:35:32 +0500

    card footer text

--- src/components/Taskscard.js
+++ src/components/Taskscard.js
@@ -5,7 +5,16 @@ import { s, vs } from 'react-native-size-matters';
 const Taskscard = () => {
   return (
     <View style={styles.cont}>
-      <Text>Taskscard</Text>
+      <View style={{ marginHorizontal: s(20) }}>
+        <View style={styles.card}>
+          <Text style={styles.cardtitle}>Wirefarming </Text>
+          <Text style={styles.cardtitle}>12:00 PM</Text>
+        </View>
+        <Text style={styles.cardescrip}>
+          {' '}
+          Make some ideation from sketch and wireframes
+        </Text>
+      </View>
     </View>
   );
 };
@@ -14,10 +23,28 @@ export default Taskscard;
 
 const styles = StyleSheet.create({
   cont: {
+    marginTop: s(10),
     width: '90%',
     height: vs(130),
     backgroundColor: '#fc569e',
     marginHorizontal: s(30),
-    borderRadius: s(25),
+    borderRadius: s(20),
+  },
+  card: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: s(20),
+  },
+  cardtitle: {
+    fontSize: s(16),
+    fontWeight: '350',
+    color: '#fff',
+  },
+  cardescrip: {
+    fontSize: s(15),
+    fontWeight: '300',
+    color: '#ffffffba',
+    marginTop:s
+    (10)
   },
 });
