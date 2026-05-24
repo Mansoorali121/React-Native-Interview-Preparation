8f9cd173bf6fe08c6c502077cd48bef7425b7dc1
Mansoor Ali <mansoorsahito601@gmail.com>
2026-05-24 09:27:00 +0500

    card ui

--- src/components/Taskscard.js
+++ src/components/Taskscard.js
@@ -1,14 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { s, vs } from 'react-native-size-matters';
 
 const Taskscard = () => {
   return (
-    <View>
+    <View style={styles.cont}>
       <Text>Taskscard</Text>
     </View>
-  )
-}
+  );
+};
 
 export default Taskscard;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  cont: {
+    width: '90%',
+    height: vs(130),
+    backgroundColor: '#fc569e',
+    marginHorizontal: s(30),
+    borderRadius: s(25),
+  },
+});
