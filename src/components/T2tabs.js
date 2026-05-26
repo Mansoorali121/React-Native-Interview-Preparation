69cad980b26ce18c8a0d5bd9205f306f01ad109e
Mansoor Ali <mansoorsahito601@gmail.com>
2026-05-26 16:32:03 +0500

    tab color changed

--- src/components/T2tabs.js
+++ src/components/T2tabs.js
@@ -42,7 +42,7 @@ const styles = StyleSheet.create({
   },
 
   activeTab: {
-    backgroundColor: colors.gradientEnd,
+    backgroundColor: colors.primary,
   },
   tabtext: {
   color: colors.black,
