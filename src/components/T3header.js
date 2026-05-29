24604b6bebe119f0181867dd3e399ffc3ed602fe
Mansoor Ali <mansoorsahito601@gmail.com>
2026-05-29 12:06:51 +0500

    t3 header complete

--- src/components/T3header.js
+++ src/components/T3header.js
@@ -1,14 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import React from 'react';
+import { vs, s } from 'react-native-size-matters';
 
 const T3header = () => {
   return (
-    <View>
-      <Text>T3header</Text>
+    <View style={styles.cont}>
+      <View style={styles.headercontainer}>
+        <TouchableOpacity style={styles.imagecircle}>
+          <Image source={require('../assets/left.png')} style={styles.image} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.imagecircle}>
+          <Image
+            source={require('../assets/search.png')}
+            style={styles.image}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
-  )
-}
+  );
+};
 
 export default T3header;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  cont: { marginTop: vs(20) },
+  image: { height: vs(28), width: s(28), fontFamily: 'Outfit-Black' },
+  headercontainer: { flexDirection: 'row', justifyContent: 'space-between' },
+  imagecircle: { padding: s(10), backgroundColor: "#f6fbff50", borderRadius: s(14) },
+});
