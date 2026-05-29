b2c57da205c29e28d070153cdf403b45d8f8b721
Mansoor Ali <mansoorsahito601@gmail.com>
2026-05-29 11:54:38 +0500

    T3 screen header

---
+++ src/components/T3header.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const T3header = () => {
+  return (
+    <View>
+      <Text>T3header</Text>
+    </View>
+  )
+}
+
+export default T3header;
+
+const styles = StyleSheet.create({})
