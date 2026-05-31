9c1eb39b4f426e46096e989e7b515e65a8878fbe
Mansoor Ali <mansoorsahito601@gmail.com>
2026-05-31 10:36:06 +0500

    update

---
+++ src/components/T3card.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const T3card = () => {
+  return (
+    <View>
+      <Text>T3card</Text>
+    </View>
+  )
+}
+
+export default T3card;
+
+const styles = StyleSheet.create({})
