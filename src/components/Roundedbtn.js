6ff2699c72264857e6e90eefbab5fba128c4cc33
Mansoor Ali <mansoorsahito601@gmail.com>
2026-06-01 15:53:33 +0500

    btn complete

--- src/components/Roundedbtn.js
+++ src/components/Roundedbtn.js
@@ -1,11 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
-import { vs } from 'react-native-size-matters';
+import { vs, s } from 'react-native-size-matters';
 
 const Roundedbtn = () => {
   return (
     <View style={styles.container}>
-      <Text>Roundedbtn</Text>
+      <TouchableOpacity style={styles.btn} >
+        <Image source={require("../assets/add.png")} style={styles.image} />
+      </TouchableOpacity>
     </View>
   )
 }
@@ -14,4 +16,21 @@ export default Roundedbtn;
 
 const styles = StyleSheet.create({
     container:{marginTop:vs(20)},
+    btn:{height:vs
+        (55),
+        backgroundColor:"#0000",
+        width:vs(55),
+        borderRadius:vs(27),
+        position:"absolute",
+        right:vs
+        (130),
+        top:vs
+        (40),
+        borderWidth:s
+        (0.2),
+        borderColor:"#000",
+    },
+    image:{height:vs(30),width:vs(30),alignSelf:"center",justifyContent:"center",marginVertical:vs
+        (13)
+    },
 })
