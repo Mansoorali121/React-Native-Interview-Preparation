1dfd4480442a3126aec4d5a924181c07ea5551de
Mansoor Ali <mansoorsahito601@gmail.com>
2026-06-07 10:26:42 +0500

    category tabs complete

--- src/components/Htabs.js
+++ src/components/Htabs.js
@@ -1,21 +1,52 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
 import { s, vs } from 'react-native-size-matters';
 
 const Htabs = () => {
+  const tabs = ['Salads', 'Hot sale', 'Popularity'];
+  const [selectedtab, setSelectedtab] = useState('Salads');
   return (
     <View style={styles.container}>
-        <TouchableOpacity style={styles.tab}>
-                  <Text>Salads</Text>
-
-        </TouchableOpacity>
+      {tabs.map((tabname, index) => (
+        <View style={{ flexDirection: 'row', gap: s(20) }}>
+          <TouchableOpacity onPress={()=>setSelectedtab(tabname)}
+            style={[
+              styles.tab,
+              selectedtab === tabname && { backgroundColor: '#FF7A45' },
+            ]}
+            key={index}
+          >
+            <Text
+              style={[
+                styles.tabtext,
+                selectedtab === tabname && { color: '#fff' },
+              ]}
+            >
+              {tabname}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      ))}
     </View>
-  )
-}
+  );
+};
 
 export default Htabs;
 
 const styles = StyleSheet.create({
-    container:{marginTop:vs(25),marginHorizontal:s(20)},
-    tab:{},
-})
+  container: {
+    marginTop: vs(25),
+    marginHorizontal: s(20),
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  tab: {
+    backgroundColor: '#eee',
+    paddingVertical: vs(10),
+    paddingHorizontal:s(20),
+    alignSelf:"flex-start",
+    alignItems: 'center',
+    borderRadius: s(20),
+  },
+  tabtext: { color: '#0000', fontFamily: 'Outfit-Light' },
+});
