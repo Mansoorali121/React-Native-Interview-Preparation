a302dff8594b8c47713aa6ce8ffd1b14771bd080
Mansoor Ali <mansoorsahito601@gmail.com>
2026-06-19 11:20:55 +0500

    footer description add

--- src2/components/Tfootercard.js
+++ src2/components/Tfootercard.js
@@ -1,6 +1,7 @@
 import { Image, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { s, vs } from 'react-native-size-matters';
+import Cfootercard from './Cfootercard';
 
 const Tfootercard = () => {
   return (
@@ -12,30 +13,35 @@ const Tfootercard = () => {
           <Image source={require('../assets/heart.png')} style={styles.image} />
         </View>
       </View>
-              <Text style={styles.price}>$8.20</Text>
-           {/* List values */}
-           <View style={styles.listcontainer}>
-               <View style={styles.item1}>
-                
-                          <Image source={require('../assets/star.png')} style={styles.image} />
-                          <Text style={styles.value}>4.6</Text>
-
-              </View>
-               <View style={styles.item1}>
-                
-                          <Image source={require('../assets/clock.png')} style={styles.image} />
-                          <Text style={styles.value}>20-25 min</Text>
-
-              </View>
-               <View style={styles.item1}>
-                
-                          <Image source={require('../assets/fire.png')} style={styles.image} />
-                                                    <Text style={styles.value}>110 Kcal</Text>
-
-
-              </View>
-           </View>
-
+      <Text style={styles.price}>$8.20</Text>
+      {/* List values */}
+      <View style={styles.listcontainer}>
+        <View style={styles.item1}>
+          <Image source={require('../assets/star.png')} style={styles.image} />
+          <Text style={styles.value}>4.6</Text>
+        </View>
+        <View style={styles.item1}>
+          <Image source={require('../assets/clock.png')} style={styles.image} />
+          <Text style={styles.value}>20-25 min</Text>
+        </View>
+        <View style={styles.item1}>
+          <Image source={require('../assets/fire.png')} style={styles.image} />
+          <Text style={styles.value}>110 Kcal</Text>
+        </View>
+      </View>
+      {/* Footer Card Values */}
+      <View style={styles.footeritem}>
+        <Cfootercard />
+      </View>
+      <View style={styles.footertext}>
+        <Text style={styles.title}>Description</Text>
+        <Text style={styles.info}>
+          Burger bang delievers mouthwatering,freshly grilled burgers packed
+          with flavor. Enjoy juicy patties, fresh toppings, and signature
+          sacues, all made to satisfy your cravings and bring you the ultimate
+          burger experince.
+        </Text>
+      </View>
     </View>
   );
 };
@@ -56,13 +62,25 @@ const styles = StyleSheet.create({
     borderRadius: s(20),
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop:s(-20)
+    marginTop: s(-20),
   },
-  description:{color:"gray",fontFamily:"Outfit-Thin"},
-  price:{fontSize: s(20), fontFamily: 'Outfit-Regular'},
-  item1:{marginTop:vs(15),flexDirection:"row",gap:s(10), alignItems:"center"
+  description: { color: 'gray', fontFamily: 'Outfit-Thin' },
+  price: { fontSize: s(20), fontFamily: 'Outfit-Regular' },
+  item1: {
+    marginTop: vs(15),
+    flexDirection: 'row',
+    gap: s(10),
+    alignItems: 'center',
   },
-  value:{fontSize: s(14), fontFamily: 'Outfit-Regular'},
-  listcontainer:{flexDirection:"row",justifyContent:"space-between"},
+  value: { fontSize: s(14), fontFamily: 'Outfit-Regular' },
+  listcontainer: { flexDirection: 'row', justifyContent: 'space-between' },
+  footeritem: {
+    marginTop: vs(20),
+    borderWidth: s(0.3),
+    borderColor: 'gray',
+    borderRadius: s(10),
+  },
+  footertext: { marginTop: vs(10) },
+  title:{fontSize: s(20), fontFamily: 'Outfit-Regular' },
+  info:{color: '#6e5d5d', fontFamily: 'Outfit-Thin',fontSize:s(14)},
 });
-
